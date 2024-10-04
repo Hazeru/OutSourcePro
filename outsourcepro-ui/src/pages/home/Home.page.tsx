0bdf2111613f85c7707e4f241bc4a18183efdeb2
Rowe <130283000+RoweRui@users.noreply.github.com>
2024-10-04 15:39:15 +0800

    Add files via upload

---
+++ outsourcepro-ui/src/pages/home/Home.page.tsx
@@ -0,0 +1,352 @@
+import "./home.scss";
+const Home = () => {
+return (
+<>
+
+<div className="home-one" style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+  <div style={{ flex: 1, textAlign: "left" }}>
+    <h1><center>Welcome To OutSourcePro</center></h1><br></br>
+        <p>
+          At OutSourcePro, we specialize in delivering tailored IT outsourcing
+          services that drive innovation, efficiency, and growth for your business.
+          Whether you need cutting-edge software development, comprehensive IT support,
+          or seamless cloud management, our team of experienced professionals is here
+          to extend your capabilities and optimize your operations. With a focus on
+          quality, scalability, and security, we help you stay ahead in a competitive
+          landscape, allowing you to focus on what matters most – growing your business.
+          Partner with us to unlock the full potential of your IT infrastructure.
+        </p>
+        <div style={{ display: "flex", justifyContent: "space-between" }}>
+          <div style={{ flex: 1, textAlign: "left" }}>
+            <img src="peeps.png" alt="OutSourcePro" style={{ width: "100%", maxWidth: "600px" }} />
+          </div>
+          <div style={{ flex: 1, textAlign: "center" }}>
+            <img src="arrow.png" alt="OutSourcePro" style={{ width: "100%", maxWidth: "300px" }} />
+          </div>
+          <div style={{ flex: 1, textAlign: "right" }}>
+            <img src="future.png" alt="OutSourcePro" style={{ width: "100%", maxWidth: "600px" }} />
+          </div>
+      </div>
+      </div>
+      </div>
+
+  <br></br>
+  <br></br>
+  <hr></hr>
+  <br></br>
+  <br></br>
+
+  <div style={{ flex: 1, textAlign: "right", border: "5px solid black", padding: "10px", display: "inline-block" }}>
+  <img src="hire.png" alt="OutSourcePro" style={{ width: "500%", height: "auto", maxWidth: "1600px" }} />
+</div>
+
+
+  <br></br>
+  <br></br>
+  <hr></hr>
+  <br></br>
+  <br></br>
+<div className="home-one" style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+  <div style={{ flex: 1, textAlign: "left" }}>
+    <h1>Hire the Top 3% of Freelance Talent®</h1><br></br>
+    <p>
+    OutSourcePro is an exclusive network of the top freelance software developers, designers, marketing experts, finance experts, product managers, and project managers in the world. 
+    Top companies hire OutSourcePro freelancers for their most important projects. 
+    </p>
+  </div>
+  <div style={{ flex: 1, textAlign: "right" }}>
+    <img src="t1.png" alt="OutSourcePro" style={{ width: "100%", maxWidth: "700px" }} />
+  </div>
+  </div>
+
+  <br></br>
+  <br></br>
+  <hr></hr>
+  <br></br>
+  <br></br>
+
+  <div className="home-one" style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+  <div style={{ flex: 1, textAlign: "left" }}>
+    <img src="t2.png" alt="OutSourcePro" style={{ width: "300%", maxWidth: "700px" }} />
+  </div>
+  <div style={{ flex: 1, textAlign: "left" }}>
+    <h1>Build Smarter Solutions, Drive Innovation</h1><br></br>
+    <p>
+    In the fast-paced world of Information Technology, creating smart, efficient solutions is the key to staying ahead. 
+    By leveraging cutting-edge tools and innovative thinking, we push the boundaries of what's possible—enhancing productivity, streamlining processes, and transforming challenges into opportunities for growth. 
+    From coding to problem-solving, we're committed to delivering technology that drives progress and builds a brighter future.<br></br><br></br>
+    ➤ You can find IT professionals for all types of projects, including web development, software engineering, and cybersecurity consulting.  <br></br>
+    ➤ You can access tools for project management, code repositories, and performance monitoring to ensure smooth and efficient development.  <br></br>
+    ➤ You can also filter developers by their experience and specialization to find someone who meets your technical requirements and budget.
+    </p>
+  </div>
+  </div>
+
+  <br></br>
+  <br></br>
+  <hr></hr>
+  <br></br>
+  <br></br>
+
+  <div className="home-one" style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+  <div style={{ flex: 1, textAlign: "left" }}>
+    <h1>Expert Freelancers Hand-Picked for Your Projects</h1><br></br>
+    <p>
+    If you want to hire from the top 3% of talent in the world for your projects, OutSourcePro is for you. Unlike other outsourcing websites, OutSourcePro has a very strict and thorough vetting process. 
+    They pick just the top 3% of freelancers who apply to make sure they’re getting only the real experts in their fields. 
+    You can hire experts in software development, design, marketing, finance, product management, and project management. But here, you don’t have to search for freelancers yourselves. 
+    OutSourcePro gives you hand-picked freelancers according to your needs.
+
+    Freelancers on OutSourcePro have to go through a strict five-step process that includes interviews, skill assessments, and test projects. And only the best of the best are accepted.
+    The hiring process doesn’t take much time. You can hire quickly in under 48 hours.
+    OutSourcePro also offers a no-risk trial period. You can work with the freelancer for a few weeks, and if you’re not satisfied, you don’t have to pay. 
+    This is a really helpful feature to let you see if the freelancer is a good fit before fully committing.
+    OutSourcePro is not for you if you’ve a limited or tight budget because all these top experts come at a really high price. 
+    There is even a mandatory deposit of at least $500 before they start looking for a freelancer for your project.
+    </p>
+  </div>
+  <div style={{ flex: 1, textAlign: "right" }}>
+    <img src="t3.png" alt="OutSourcePro" style={{ width: "100%", maxWidth: "700px" }} />
+  </div>
+  </div>
+
+  <br></br>
+  <br></br>
+  <hr></hr>
+  <br></br>
+  <br></br>
+
+<div className="home-one" style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+<div style={{ flex: 1, textAlign: "left" }}>
+    <img src="t4.png" alt="OutSourcePro" style={{ width: "100%", maxWidth: "700px" }} />
+  </div>
+  <div style={{ flex: 1, textAlign: "left" }}>
+    <h1>Make it real with OutSourcePro</h1><br></br>
+    <p>
+    <br></br><b>The best talent</b><br></br>
+    ➤Discover reliable professionals by exploring their portfolios and immersing yourself in the feedback shared on their profiles.<br></br>
+    <br></br><b>Fast bids</b><br></br>
+    ➤Get quick, no-obligation quotes from skilled freelancers. 80% of jobs receive bids within 60 seconds. Your idea is just moments from reality.<br></br>
+    <br></br><b>Quality work</b><br></br>
+    ➤With Freelancer's talent pool of over 60 million professionals at your fingertips, you'll find quality talent to get what you need done.<br></br>
+    <br></br><b>Be in control</b><br></br>
+    ➤Stay in the loop while on the move. Chat with your freelancers and get real time updates with our mobile app. Anytime, anywhere.<br></br>
+   </p>
+  </div>
+  </div>
+
+  <br></br>
+  <br></br>
+  <hr></hr>
+  <br></br>
+  <br></br>
+
+<div className="home-one" style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+  <div style={{ flex: 1, textAlign: "left" }}>
+    <h1>OutSourcePro matches you with the right freelancers based on your needs to reduce your manual search work</h1>
+    <br></br>
+    <p>
+    You can post a job with all your requirements on the website. It then matches the project with a list of freelancers that specialise in that field of work and shortlists candidates for you. 
+    Any interested freelancers can also see your job and submit their proposals to you.
+
+    If you prefer to look for freelancers manually, you can do that as well. While searching, you can add filters like what skills you're looking for, experience level, hourly rate, etc. 
+
+    Upwork’s Project Catalog lets freelancers offer set packages like a logo design for $100. This is great if you need a quick solution for something and want to know the price right away.
+    Depending on the project, you can hire freelancers on either an hourly basis or for a fixed price.
+    There’s also a messaging system, file-sharing options, and built-in video calls. This makes it easy to stay in touch with freelancers and manage projects without switching between apps.
+    Before you use UpWork, there are a few things you should know about. UpWork charges service fees on both sides, which can make projects more expensive. Also, while it’s a good thing that there are many talented freelancers, it can be time-consuming to go through so many portfolios and profiles before you find a suitable hire.
+      
+  </p>
+  </div>
+  <div style={{ flex: 1, textAlign: "right" }}>
+    <img src="t5.png" alt="OutSourcePro" style={{ width: "100%", maxWidth: "700px" }} />
+  </div>
+  </div>
+
+  <br></br>
+  <br></br>
+  <hr></hr>
+  <br></br>
+  <br></br>
+
+{/* SECOND SECTION */}
+
+  <div className="home-TWO" style={{ display: "flex", justifyContent: "space-between", alignItems: "center", gap: "20px" }}>
+  <div style={{ flex: "1", textAlign: "center" }}>
+    <h1> Leverage World-Class Talent</h1>
+    <p>
+    We are the largest, globally-distributed network of top business, design, and technology talent, ready to tackle your most important initiatives.
+    </p>
+  </div>
+  </div>
+
+  <br></br>
+  <br></br>
+
+<div className="home-four" style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '20px' }}>
+
+<div className="sec3" style={{ flex: 1, textAlign: 'center', margin: '0 10px' }}>
+    <div style={{ flex: "0 0 auto", maxWidth: "300px", margin: '0 auto' }}>
+      <img src="dev.png" alt="OutSourcePro" style={{ width: "100%",  maxWidth: "100px" }} />
+      <p style={{ margin: '0', textAlign: 'center' }}>
+      <br></br>
+      <b>Developers</b>
+      <br></br>
+      Seasoned software engineers, coders, and architects with expertise across hundreds of technologies.
+      </p>
+    </div>
+  </div>
+
+
+  <div className="sec3" style={{ flex: 1, textAlign: 'center', margin: '0 10px' }}>
+    <div style={{ flex: "0 0 auto", maxWidth: "300px", margin: '0 auto' }}>
+      <img src="des.png" alt="OutSourcePro" style={{ width: "100%", maxWidth: "100px" }} />
+      <p style={{ margin: '0', textAlign: 'center' }}>
+      <br></br>
+      <b>Designers</b>
+      <br></br>
+      Expert UI, UX, Visual, and Interaction designers as well as a wide range of illustrators, animators, and more.      </p>
+    </div>
+  </div>
+
+  <div className="sec3" style={{ flex: 1, textAlign: 'center', margin: '0 10px' }}>
+    <div style={{ flex: "0 0 auto", maxWidth: "300px", margin: '0 auto' }}>
+      <img src="inv.png" alt="OutSourcePro" style={{ width: "100%",  maxWidth: "100px" }} />
+      <p style={{ margin: '0', textAlign: 'center' }}>
+      <br></br>
+      <b>Finance Experts</b>
+      <br></br>
+      Experts in financial modeling & valuation, startup funding, interim CFO work, and market sizing.
+      </p>
+    </div>
+  </div>
+
+  <div className="sec3" style={{ flex: 1, textAlign: 'center', margin: '0 10px' }}>
+    <div style={{ flex: "0 0 auto", maxWidth: "300px", margin: '0 auto' }}>
+      <img src="pd.png" alt="OutSourcePro" style={{ width: "100%", maxWidth: "100px" }} />
+      <p style={{ margin: '0', textAlign: 'center' }}>
+      <br></br>
+      <b>Project Managers</b>
+      <br></br>
+      Digital and technical project managers, scrum masters, and more with expertise in numerous PM tools, frameworks, and styles.
+      </p>
+    </div>
+  </div>
+
+<div className="sec3" style={{ flex: 1, textAlign: 'center', margin: '0 10px' }}>
+    <div style={{ flex: "0 0 auto", maxWidth: "300px", margin: '0 auto' }}>
+      <img src="market.png" alt="OutSourcePro" style={{ width: "100%",  maxWidth: "100px" }} />
+      <p style={{ margin: '0', textAlign: 'center' }}>
+      <br></br>
+      <b>Marketing Experts</b>
+      <br></br>
+      Experts in digital marketing, growth marketing, content creation, market research, brand strategy execution, social media marketing, and more.
+      </p>
+    </div>
+  </div>
+</div>
+
+  <br></br>
+  <br></br>
+  <hr></hr>
+  <br></br>
+  <br></br>
+
+<div className="home-four" style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '20px' }}>
+  {/* Section 1 */}
+  <h1>HIRING MADE EASY:</h1><br></br>
+  <div className="sec1" style={{ flex: 1, textAlign: 'center', margin: '0 10px' }}>
+    <div style={{ flex: "0 0 auto", maxWidth: "300px", margin: '0 auto' }}>
+      <img src="1.png" alt="OutSourcePro" style={{ width: "60%", maxWidth: "300px" }} />
+      <br></br><h3>Talk To Our Industry Experts</h3><br></br>
+      <p style={{ margin: '0', textAlign: 'center' }}>
+      An expert on our team will work with you to understand your goals, technical needs, and team dynamics.
+      </p>
+    </div>
+  </div>
+
+  {/* Section 2 */}
+  <div className="sec2" style={{ flex: 1, textAlign: 'center', margin: '0 10px' }}>
+    <div style={{ flex: "0 0 auto", maxWidth: "300px", margin: '0 auto' }}>
+    <img src="2.png" alt="OutSourcePro" style={{ width: "60%", maxWidth: "300px" }} />
+    <br></br><h3>Work With Hand-Selected Talent</h3><br></br>
+      <p style={{ margin: '0', textAlign: 'center' }}>
+        Within days, we'll introduce you to the right talent for your project. Average time to match is under 24 hours.
+      </p>
+    </div>
+  </div>
+
+  {/* Section 3 */}
+  <div className="sec3" style={{ flex: 1, textAlign: 'center', margin: '0 10px' }}>
+    <div style={{ flex: "0 0 auto", maxWidth: "300px", margin: '0 auto' }}>
+    <img src="3.png" alt="OutSourcePro" style={{ width: "60%", maxWidth: "300px" }} />
+    <br></br><h3>The Right Fit, Guaranteed</h3><br></br>
+      <p style={{ margin: '0', textAlign: 'center' }}>
+      Work with your new team member on a trial basis (pay only if satisfied), ensuring you hire the right people for the job.
+      </p>
+    </div>
+  </div>
+
+</div>
+
+  <br></br>
+  <br></br>
+  <hr></hr>
+  <br></br>
+  <br></br>
+
+<h1><center>Build Amazing Teams, On Demand
+<br></br>Quickly assemble the teams you need, exactly when you need them.</center>
+
+</h1>
+  <div className="home-four" style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', padding: '20px', gap: '20px' }}>
+  
+  <div className="sec1" style={{ textAlign: 'center', maxWidth: '600px', margin: '20px' }}>
+  <img src="hirecan.png" alt="OutSourcePro" style={{ width: "60%", maxWidth: "70px" }} />  
+  <p><b>Hire in under 48 hours.</b></p>
+  <p>HScale up or down, no strings attached. We offer flexible engagements from hourly to full-time.</p>
+  </div>
+
+  <div className="sec1" style={{ textAlign: 'center', maxWidth: '600px', margin: '20px' }}>
+  <img src="top3.png" alt="OutSourcePro" style={{ width: "60%", maxWidth: "70px" }} />  
+  <p><b>The Top 3%</b></p>
+  Every applicant to the OutSourcePro network is rigorously tested and vetted. Our highly selective process leads to a 98% trial-to-hire success rate.
+  </div>
+
+  <div className="sec2" style={{ textAlign: 'center', maxWidth: '600px', margin: '20px' }}>
+  <img src="lead.png" alt="OutSourcePro" style={{ width: "60%", maxWidth: "70px" }} />  
+  <p><b>Leading the Future of Work</b></p>
+  Our network is ready for tomorrow's business challenges by embracing advanced and specialized skills including blockchain and AI.
+  </div>
+  <div className="sec3" style={{ textAlign: 'center', maxWidth: '600px', margin: '20px' }}>
+  <img src="level.png" alt="OutSourcePro" style={{ width: "60%", maxWidth: "70px" }} />  
+  <p><b>A Level Above</b></p>
+  Every single freelancer in our global network embodies the highest levels of integrity, professionalism, and communication. 
+  </div>
+</div>
+
+<br></br>
+<br></br>
+<br></br>
+
+  <footer className="footer">
+  <div className="footer-content" style={{ display: 'flex', backgroundColor: "black", borderTop: "1px solid white", fontSize: '14px', justifyContent: 'space-between', alignItems: 'center', padding: '20px', color: 'white' }}>
+    <div className="footer-text" style={{ textAlign: 'left' }}>
+      OutSourcingPro is a proudly owned subsidiary of Rowe Group
+      <br />
+      <span style={{ textAlign: 'center', display: 'block' }}>© Copyright 2015 - 2024 OutSourcingPro International Ltd. All Rights Reserved</span>
+    </div>
+    <div className="footer-links" style={{ textAlign: 'right' }}>
+      <a href="#"  style={{ color: 'white' }}>Privacy Policy</a>  |
+      <a href="#"  style={{ color: 'white' }}>About Us</a>  |
+      <a href="#"  style={{ color: 'white' }}>Contact Us</a>
+    </div>
+    </div>
+</footer>
+
+
+    </>
+  );
+};
+
+export default Home;
